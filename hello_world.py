35526103572349029a8e215b6748d63b94e28ceb
rhelma <elias.maki@tuni.fi>
2025-03-11 18:06:45 +0200

    Removed extra rows from 'hello_world.py'

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,2 @@
 print("Hello world!")
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-=======
->>>>>>> git-inter-normal-merge/master
-=======
 print("Hello once again")
->>>>>>> git-inter-normal-merge/feature/add-logging
